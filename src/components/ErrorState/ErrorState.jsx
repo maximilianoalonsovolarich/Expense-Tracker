871fb485e7200b292515aa9d6fe86bbc2eb3e0c9
maximilianoalonsovolarich <maximilianoalonso2012.ma@gmail.com>
2024-06-21 22:57:14 -0500

    fechas

--- src/components/ErrorState/ErrorState.jsx
+++ src/components/ErrorState/ErrorState.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import {
-  Alert,
-  AlertIcon,
-  AlertTitle,
-  AlertDescription,
-} from '@chakra-ui/react';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
 import './ErrorState.css';
 
 const ErrorState = () => (
-  <Alert status="error">
-    <AlertIcon />
+  <Alert severity="error">
     <AlertTitle>An error occurred!</AlertTitle>
-    <AlertDescription>Please try again later.</AlertDescription>
+    Please try again later.
   </Alert>
 );
 
